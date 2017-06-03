94f314d3f3d5d2540d7a8dfadb69f7620e9cda5b
marshmallow.in.cocoa.latte <marshmallow.in.cocoa.latte@gmail.com>
2017-06-04 01:00:40 +0900

    フィールドシーン、プレイヤーキャラクター追加

---
+++ OpenFieldRPG/Assets/Scripts/FieldScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+//!	@brief	フィールドシーン
+public class FieldScene : MonoBehaviour
+{
+	//!	@brief	開始処理
+	//!	@return	なし
+	void Start()
+	{
+		if( _playerModel != null ) {
+			_player.create(new Player.PlayerCreateParam() {
+				model	= _playerModel,
+				anim	= _playerModel.GetComponent<Animator>(),
+			});
+			_player.initialize();
+		}
+	}
+	
+	//!	@brief	更新処理
+	//!	@return	なし
+	void Update()
+	{
+		_player.update();
+	}
+
+	[SerializeField] private GameObject		_playerModel	= null;				//!< プレイヤーモデル
+
+	private Player							_player			= new Player();		//!< プレイヤー
+}
---
+++ OpenFieldRPG/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Player
+{
+	//!	@brief	プレイヤー作成情報
+	public struct PlayerCreateParam
+	{
+		public GameObject	model;		//!< モデル
+		public Animator		anim;		//!< アニメーション制御
+	}
+
+	private const float				SP_STAY_ANIM_TIME		= 5.0f;									//!< 特殊待機モーションを再生するまでの時間
+	private static readonly int		ANIM_TRIG_SP_STAY		= Animator.StringToHash("spStay");		//!< 特殊待機モーション再生アニメーショントリガー
+
+	//!	@brief	作成
+	//!	@param	[in]	param	プレイヤー作成情報
+	//!	@return	なし
+	public void create(PlayerCreateParam param)
+	{
+		_model	= param.model;
+		_anim	= param.anim;
+	}
+
+	//!	@brief	初期化
+	//!	@return	なし
+	public void initialize()
+	{
+		_stayTimer = SP_STAY_ANIM_TIME;
+	}
+
+	//!	@brief	更新
+	//!	@return	なし
+	public void update()
+	{
+		_stayTimer -= Time.deltaTime;
+		if( _stayTimer < 0.0f ) {
+			_stayTimer = SP_STAY_ANIM_TIME;
+			setAnimTrigger(ANIM_TRIG_SP_STAY);
+		}
+	}
+
+	//!	@brief	アニメーショントリガーの設定
+	//!	@param	[in]	id		パラメーターID
+	//!	@return	なし
+	private void setAnimTrigger(int id)
+	{
+		if( _anim == null ) return;
+
+		_anim.SetTrigger(id);
+	}
+
+	private GameObject		_model		= null;		//!< プレイヤーモデル
+	private Animator		_anim		= null;		//!< アニメーション制御
+
+	private float			_stayTimer	= 0.0f;		//!< 待機時間タイマー
+}
